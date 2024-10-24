177bcd98f65ce21ba2a276c92e75d556d13b9c95
Lennart Guggenberger <lennart-luis.guggenberger@hsrw.org>
2024-10-25 00:04:23 +0200

    Vapor-Server für Sitzung-Service generiert -Vier Controller angelegt für parallele Zusammenarbeit -#45

---
+++ backend/meeting-service/Sources/App/Controllers/AttendanceController.swift
@@ -0,0 +1,8 @@
+import Fluent
+import Vapor
+
+struct AttendanceController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+}
---
+++ backend/meeting-service/Sources/App/Controllers/VotingController.swift
@@ -0,0 +1,8 @@
+import Fluent
+import Vapor
+
+struct VotingController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+}
