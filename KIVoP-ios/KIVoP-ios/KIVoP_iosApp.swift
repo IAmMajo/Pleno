c9cb6b7b5eb4b11a4ceeac5c437c035b32c88a16
saboq <amine.ahamri@hsrw.org>
2024-10-29 16:07:01 +0100

    Creating XCode Project -@30078 -#66

---
+++ KIVoP-ios/KIVoP-ios/KIVoP_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KIVoP_iosApp.swift
+//  KIVoP-ios
+//
+//  Created by Amine Ahamri on 29.10.24.
+//
+
+import SwiftUI
+
+@main
+struct KIVoP_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KIVoP-ios/KIVoP-iosTests/KIVoP_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  KIVoP_iosTests.swift
+//  KIVoP-iosTests
+//
+//  Created by Amine Ahamri on 29.10.24.
+//
+
+import Testing
+@testable import KIVoP_ios
+
+struct KIVoP_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
