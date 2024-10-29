958684e8e83c01faa86544a63fa1e1c9102b8ccb
30044 <hanna.steffen@hsrw.org>
2024-10-29 16:07:01 +0100

    added text to ContentView + created folders: Views, Data, Utils

---
+++ KIVoP-ios/KIVoP-ios/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KIVoP-ios
+//
+//  Created by Amine Ahamri on 29.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! Der Test Branch funktioniert!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
