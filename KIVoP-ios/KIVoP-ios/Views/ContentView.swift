13e459f052d858cbf18f6047bea054fc8c50d0e9
30044 <hanna.steffen@hsrw.org>
2024-10-29 21:53:20 +0100

    ContentView Text geändert

--- KIVoP-ios/KIVoP-ios/Views/ContentView.swift
+++ KIVoP-ios/KIVoP-ios/Views/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! Der Test Branch funktioniert!")
+            Text("Hello, world!")
         }
         .padding()
     }
