0843dfcdaf697886c3fe70366ecb849c95472a75
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-11-01 01:59:50 +0100

    fixed some small problems -recycle typedArray -removed never used import -#63

--- app/src/main/java/com/example/kivopandriod/AgendaCard.kt
+++ app/src/main/java/com/example/kivopandriod/AgendaCard.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.compose.ui.graphics.Color
@@ -29,6 +28,8 @@ class AgendaCard @JvmOverloads constructor(
 
             setCardColor(cardColor)
             setCardFontColor(cardFontColor)
+
+            typedArray.recycle()
         }
 
     }
