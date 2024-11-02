8d90df2d819f03080424ad11234fa485ea6e7a3c
30054 <henrik.peltzer@hsrw.org>
2024-11-02 14:58:31 +0100

    adding attendance view - all needed views with default content - AnwesenheitView mit hardcoded Testdaten - #80

---
+++ KIVoP-ios/KIVoP-ios/Views/Anwesenheit/AnwesenheitAktuellView.swift
@@ -0,0 +1,18 @@
+//
+//  AnwesenheitAktuellView.swift
+//  KIVoP-ios
+//
+//  Created by Henrik Peltzer on 02.11.24.
+//
+
+import SwiftUI
+
+struct AnwesenheitAktuellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnwesenheitAktuellView()
+}
