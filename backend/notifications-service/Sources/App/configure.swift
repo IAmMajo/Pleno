e60221ca095af2140f7d10ccb3893e81173e2b62
Majo <git@maxoverlack.dev>
2024-11-10 00:16:37 +0100

    #50 Add Leaf

--- backend/notifications-service/Sources/App/configure.swift
+++ backend/notifications-service/Sources/App/configure.swift
@@ -1,12 +1,15 @@
 import NIOSSL
 import Fluent
 import FluentPostgresDriver
+import Leaf
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.views.use(.leaf)
 
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
