5b7ab7ede892858ff2ff3fb3800af854c8a5273f
Tunnissen, Jan <jan.tunnissen@hsrw.org>
2024-11-10 14:35:34 +0100

    fix copy und paste fehler - #72

--- backend/auth-service/Sources/App/Migrations/CreateIdentityHistory.swift
+++ backend/auth-service/Sources/App/Migrations/CreateIdentityHistory.swift
@@ -5,7 +5,6 @@ struct CreateIdentityHistory: AsyncMigration {
     func prepare(on database: Database) async throws {
         try await database.schema(IdentityHistory.schema)
             .id()
-            .field("name", .string, .required)
             .field("user_id", .uuid, .required, .references(User.schema, "id"))
             .field("identity_id", .uuid, .required, .references(Identity.schema, "id"))
             .field("valid_from", .date)
