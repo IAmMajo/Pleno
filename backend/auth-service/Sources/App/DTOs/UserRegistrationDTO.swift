abe128aa4bc99b1f05fa930113ec388c259faba7
Tunnissen, Jan <jan.tunnissen@hsrw.org>
2024-11-10 14:37:55 +0100

    DTO fuer Registrierung angelegt - #72

---
+++ backend/auth-service/Sources/App/DTOs/UserRegistrationDTO.swift
@@ -0,0 +1,17 @@
+import Fluent
+import Vapor
+import Models
+
+public struct UserRegistrationDTO: Content {
+    public var name: String?
+    public var email: String?
+    public var password: String?
+}
+
+extension UserRegistrationDTO: Validatable {
+    static public func validations(_ validations: inout Validations) {
+        validations.add("name", as: String.self, is: !.empty)
+        validations.add("email", as: String.self, is: .email)
+        validations.add("password", as: String.self, is: !.empty)
+    }
+}
