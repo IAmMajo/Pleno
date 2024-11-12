f8dab1119a8c00a3603bce2308b42a9aa9f36c5c
maxi10807 <maximilianeinhaus@gmail.com>
2024-11-12 16:40:11 +0100

    Anpassung Auth-Service -AuthMiddleware hinzugefügt -Route zur Verifizierung des JWT-Tokens -Anpassung config-file -#89

--- backend/auth-service/Sources/App/Controllers/AuthController.swift
+++ backend/auth-service/Sources/App/Controllers/AuthController.swift
@@ -1,13 +1,26 @@
 import Fluent
 import Vapor
 import Models
+import JWT
 
 struct AuthController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
+        let jwtSigner = JWTSigner.hs256(key: "Ganzgeheimespasswort")
+        let authMiddleware = AuthMiddleware(jwtSigner: jwtSigner)
+        // Auth-Routen
         let authRoutes = routes.grouped("auth");
         authRoutes.post("login", use: self.login)
-        
+        // geschützte Auth-Routen
+        let protectedRoutes = authRoutes.grouped(authMiddleware)
+        protectedRoutes.get("token-verify", use: self.verifyJWTToken)
+        protectedRoutes.get("test", use: self.test)
     }
+    
+    @Sendable
+    func test(req: Request) async throws -> String {
+        return "Das hier ist eine Test-Antwort"
+    }
+    
     @Sendable
     func login(req: Request) async throws -> TokenResponseDTO {
         let loginRequest = try req.content.decode(UserLoginDTO.self)
@@ -29,5 +42,30 @@ struct AuthController: RouteCollection {
         
         let tokenResponse = TokenResponseDTO(token: token)
         return tokenResponse
+    }
+    
+    @Sendable
+    func verifyJWTToken(req: Request) async throws -> HTTPResponseStatus {
+        let jwtSigner = JWTSigner.hs256(key: "Ganzgeheimespasswort")
+        
+        guard let token = req.headers.bearerAuthorization?.token ?? req.cookies["token"]?.string else {
+            req.logger.warning("No Token found in Authorization header or cookies")
+            return .unauthorized
+        }
+        
+        do {
+            let payload = try jwtSigner.verify(token, as: JWTPayloadDTO.self)
+            
+            req.logger.info("Token verified successfully for user \(payload.userID!)")
+            return .ok
+        } catch {
+            req.logger.error("Token verification failed: \(error)")
+            
+            if let jwtError = error as? JWTError, case .claimVerificationFailure(_,_) = jwtError {
+                return .forbidden
+            } else {
+                return .internalServerError
+            }
+        }
     }
 }
