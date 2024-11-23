a6422cc908127807fd3fcfe9513eec761f72b762
Adrian Paus <adrian.paus@hsrw.org>
2024-11-23 15:10:48 +0100

    DTOs zu MeetingsView hinzugefügt. Meetings werden nun dynamisch erzeugt. - @30038

--- KIVoP-ios/KIVoP-ios/Views/MainPage/MainPage.swift
+++ KIVoP-ios/KIVoP-ios/Views/MainPage/MainPage.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import MeetingServiceDTOs
 
 struct MainPage: View {
     @State private var Name: String = "Max Mustermann"
@@ -9,7 +10,148 @@ struct MainPage: View {
     }
 
     var body: some View {
-        NavigationView {
+        let exampleLocation = GetLocationDTO(
+                id: UUID(),
+                name: "Alte Turnhalle",
+                street: "Altes Grab",
+                number: "5",
+                letter: "b",
+                postalCode: "42069",
+                place: "Hölle"
+            )
+
+            let exampleChair = GetIdentityDTO(
+                id: UUID(),
+                name: "Heinz-Peters"
+            )
+
+            // Beispielmeetings erstellen
+            let exampleMeetings = [
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Vorstandsitzung Januar",
+                    description: "Eröffnung des Jahres.",
+                    status: .scheduled,
+                    start: Date().addingTimeInterval(86400 * 30), // In 30 Tagen
+                    duration: 120,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG001"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Strategiemeeting",
+                    description: "Langfristige Planung.",
+                    status: .scheduled,
+                    start: Date().addingTimeInterval(86400 * 60), // In 60 Tagen
+                    duration: 90,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG002"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Finanzreview",
+                    description: "Rückblick auf das Budget.",
+                    status: .completed,
+                    start: Date().addingTimeInterval(-86400 * 10), // Vor 10 Tagen
+                    duration: 90,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG003"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Abschlussmeeting Q4",
+                    description: "Evaluation des Quartals.",
+                    status: .completed,
+                    start: Date().addingTimeInterval(-86400 * 20), // Vor 20 Tagen
+                    duration: 120,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG004"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Jahreshauptversammlung",
+                    description: "Alle Mitglieder treffen sich.",
+                    status: .inSession,
+                    start: Date(), // Heute
+                    duration: 240,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG005"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Kickoff 2024",
+                    description: "Start ins neue Jahr.",
+                    status: .scheduled,
+                    start: Date().addingTimeInterval(86400 * 5), // In 5 Tagen
+                    duration: 180,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG006"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Marketingmeeting",
+                    description: "Planung der Kampagnen.",
+                    status: .completed,
+                    start: Date().addingTimeInterval(-86400 * 40), // Vor 40 Tagen
+                    duration: 150,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG007"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Krisenbesprechung",
+                    description: "Schnelle Reaktion erforderlich.",
+                    status: .inSession,
+                    start: Date(), // Heute
+                    duration: 60,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG008"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Treffen mit Partnern",
+                    description: "Austausch und Networking.",
+                    status: .scheduled,
+                    start: Date().addingTimeInterval(86400 * 15), // In 15 Tagen
+                    duration: 180,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG009"
+                ),
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Jubiläumssitzung",
+                    description: "Feier des 10-jährigen Bestehens.",
+                    status: .completed,
+                    start: Date().addingTimeInterval(-86400 * 70), // Vor 70 Tagen
+                    duration: 200,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG010"
+                ),
+                // Das zusätzliche Meeting
+                GetMeetingDTO(
+                    id: UUID(),
+                    name: "Team-Besprechung",
+                    description: "Wöchentliche Besprechung des Teams.",
+                    status: .inSession,
+                    start: Date(), // Heute
+                    duration: 90,
+                    location: exampleLocation,
+                    chair: exampleChair,
+                    code: "MTG011"
+                )
+            ]
+        
+        NavigationStack {
             VStack(alignment: .leading, spacing: 0) {
                 // Greeting
                 Text("Hallo, \(User)")
@@ -51,7 +193,7 @@ struct MainPage: View {
                 List {
                     // Einzeloption Sitzungen
                     Section {
-                       NavigationLink(destination: KIVoP_ios.Meeting()) {
+                       NavigationLink(destination: MeetingView(meetings: exampleMeetings)) {
                             HStack {
                                 Image(systemName: "calendar.badge.clock")
                                     .foregroundColor(.accentColor)
