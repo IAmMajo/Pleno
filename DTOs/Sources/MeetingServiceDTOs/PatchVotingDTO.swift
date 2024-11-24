3d89ee16ac8577d75e46dc18ff1799f297684df1
Lennart Guggenberger <lennart-luis.guggenberger@hsrw.org>
2024-11-24 16:30:59 +0100

    PatchVotingDTO: Kommentar -Ein Kommentar zur Klarifizierung ergänzt

--- DTOs/Sources/MeetingServiceDTOs/PatchVotingDTO.swift
+++ DTOs/Sources/MeetingServiceDTOs/PatchVotingDTO.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct PatchVotingDTO: Codable {
     public var question: String?
     public var anonymous: Bool?
-    public var options: [GetVotingOptionDTO]?
+    public var options: [GetVotingOptionDTO]? // If options is not nil: ALL previous options will be deleted/replaced
     
     public init(question: String? = nil, anonymous: Bool? = nil, options: [GetVotingOptionDTO]? = nil) {
         self.question = question
