957c79a3c9a00f8454b713de776066512b9eb51e
Lennart Guggenberger <lennart-luis.guggenberger@hsrw.org>
2024-11-24 17:02:29 +0100

    VotingDTOs: Description ergänzt -.inits angepasst -#137

--- DTOs/Sources/MeetingServiceDTOs/GetVotingDTO.swift
+++ DTOs/Sources/MeetingServiceDTOs/GetVotingDTO.swift
@@ -4,16 +4,18 @@ public struct GetVotingDTO: Codable {
     public var id: UUID
     public var meetingId: UUID
     public var question: String
+    public var description: String
     public var isOpen: Bool
     public var startedAt: Date?
     public var closedAt: Date?
     public var anonymous: Bool
     public var options: [GetVotingOptionDTO]
     
-    public init(id: UUID, meetingId: UUID, question: String, isOpen: Bool, startedAt: Date? = nil, closedAt: Date? = nil, anonymous: Bool, options: [GetVotingOptionDTO]) {
+    public init(id: UUID, meetingId: UUID, question: String, description: String, isOpen: Bool, startedAt: Date? = nil, closedAt: Date? = nil, anonymous: Bool, options: [GetVotingOptionDTO]) {
         self.id = id
         self.meetingId = meetingId
         self.question = question
+        self.description = description
         self.isOpen = isOpen
         self.startedAt = startedAt
         self.closedAt = closedAt
