3429c7cdbf4c7ebec433cda6a408e2e1f731f436
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-11-28 15:02:19 +0100

    merged staging-android into current merge #168

--- app/src/main/java/com/example/kivopandriod/pages/ResponseActivity.kt
+++ app/src/main/java/com/example/kivopandriod/pages/ResponseActivity.kt
@@ -8,6 +8,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -16,10 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.kivopandriod.components.ResponseItem
 import com.example.kivopandriod.components.ResponseList
+import com.example.kivopandriod.components.SitzungsCard
 import com.example.kivopandriod.services.responseList
+import java.time.LocalDate
 
 
 @Composable
@@ -42,7 +49,12 @@ fun ResponseScreen(
             }
             println(responses)
             // UI anzeigen
-            ResponseList(responses = responses, "Rückmeldungen")
+            val eventDate = LocalDate.of(2024, 10, 24) // 24.10.2024
+            Column {
+                SitzungsCard(title = "Vorstandswahl", date = eventDate)
+                Spacer(Modifier.size(12.dp))
+                ResponseList(responses = responses, "Rückmeldungen")
+            }
         }
     }
 
