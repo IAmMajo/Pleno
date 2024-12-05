6235328e7f1b137c898581afff9fce051a1df503
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-05 04:27:17 +0100

    added DTO's for API response data handling - #185 - #30542

---
+++ app/src/main/java/com/example/kivopandriod/moduls/GetIdentityDTO.kt
@@ -0,0 +1,8 @@
+package com.example.kivopandriod.moduls
+
+import java.util.UUID
+
+data class GetIdentityDTO (
+    var id : UUID,
+    var name : String
+)
---
+++ app/src/main/java/com/example/kivopandriod/moduls/GetVotingOptionDTO.kt
@@ -0,0 +1,6 @@
+package com.example.kivopandriod.moduls
+
+data class GetVotingOptionDTO (
+     var index : UByte,
+     var text : String
+)
