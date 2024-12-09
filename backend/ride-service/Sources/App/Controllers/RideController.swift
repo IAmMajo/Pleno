77a48832633cba59dc654e4b7df9af2058b49b3c
Tunnissen, Jan <jan.tunnissen@hsrw.org>
2024-12-09 16:18:19 +0100

    Route um an Fahrt teilzunehmen hinzu - #201

--- backend/ride-service/Sources/App/Controllers/RideController.swift
+++ backend/ride-service/Sources/App/Controllers/RideController.swift
@@ -9,6 +9,7 @@ struct RideController: RouteCollection {
         let rideRoutes = routes.grouped("rides")
         let adminRideRoutes = rideRoutes.grouped(AdminMiddleware())
         rideRoutes.get("", use: getAllRides)
+        rideRoutes.post(":id", "participate", use: newParticipate)
         adminRideRoutes.post("", use: newRide)
         
     }
@@ -35,6 +36,47 @@ struct RideController: RouteCollection {
         
         // save ride in database
         try await ride.save(on: req.db)
+        
+        return .ok
+    }
+    
+    @Sendable
+    func newParticipate(req: Request) async throws -> HTTPStatus {
+        // parse DTO
+        guard let participateDTO = try? req.content.decode(ParticipateDTO.self) else {
+            throw Abort(.badRequest, reason: "Invalid request body! Expected ParticipateDTO.")
+        }
+        
+        // validate DTO
+        if !participateDTO.isValid() {
+            throw Abort(.badRequest, reason: "ParticipateDTO is not valid!")
+        }
+        
+        // parse ride id as UUID
+        guard let ride_id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest, reason: "Invalid or missing ride ID")
+        }
+        
+        // check if ride exists
+        guard let _ = try await Ride.find(ride_id, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        // check if already participating
+        let count = try await Participant.query(on: req.db)
+            .filter(\.$user.$id == req.jwtPayload.userID)
+            .filter(\.$ride.$id == ride_id)
+            .count()
+        
+        if count != 0 {
+            throw Abort(.forbidden, reason: "User is already participating in this ride!")
+        }
+        
+        // create participant
+        let participant = Participant(rideId: ride_id, userId: req.jwtPayload.userID, driver: participateDTO.driver, passengers_count: participateDTO.passenger_count, latitude: participateDTO.latitude, longitude: participateDTO.longitude)
+        
+        // save participant
+        try await participant.save(on: req.db)
         
         return .ok
     }
---
+++ backend/ride-service/Sources/App/Extensions/DTOs/Ext_ParticipateDTO.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Vapor
+
+extension ParticipateDTO {
+    func isValid() -> Bool {
+        // wenn kein driver, passenger_count sollte nil sein
+        if !self.driver {
+            if self.passenger_count == nil {
+                return true
+            }
+            return false
+        }
+        
+        // wenn driver, passenger_count muss groesser 0 sein
+        guard let passenger_count = self.passenger_count else {
+            return false
+        }
+        
+        if passenger_count > 0 {
+            return true
+        }
+        
+        // sonst ungueltig
+        return false
+    }
+}
