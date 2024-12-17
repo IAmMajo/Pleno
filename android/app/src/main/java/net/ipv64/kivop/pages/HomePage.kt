0007faa1ec7c0b792e0138d933926d4fcaaf6c01
Gubi <evgeny.roman@hsrw.org>
2024-12-17 02:23:50 +0100

    added handle login and logout + navigation

--- android/app/src/main/java/net/ipv64/kivop/pages/HomePage.kt
+++ android/app/src/main/java/net/ipv64/kivop/pages/HomePage.kt
@@ -1,10 +1,16 @@
 package net.ipv64.kivop.pages
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import net.ipv64.kivop.handleLogout
+import net.ipv64.kivop.services.AuthController
 
 @Composable
 fun HomePage(navController: NavController) {
-  Text(text = "home")
+  //Text(text = "home")
+  Button(onClick = {
+    handleLogout(navController.context)
+  }){}
 }
