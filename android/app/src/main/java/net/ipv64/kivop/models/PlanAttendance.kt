72a5423b6c4f033342e46ca9764fcb57a7939b48
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-19 18:16:28 +0100

    Changed api to use DTOs - added put api for planning Attendance

---
+++ android/app/src/main/java/net/ipv64/kivop/models/PlanAttendance.kt
@@ -0,0 +1,6 @@
+package net.ipv64.kivop.models
+
+enum class PlanAttendance {
+  present,
+  absent,
+}
--- android/app/src/main/java/net/ipv64/kivop/services/api/AttendancesApi.kt
+++ android/app/src/main/java/net/ipv64/kivop/services/api/AttendancesApi.kt
@@ -2,67 +2,106 @@
 package net.ipv64.kivop.services.api
 
 import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import net.ipv64.kivop.dtos.MeetingServiceDTOs.AttendanceStatus
+import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetAttendanceDTO
+import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetIdentityDTO
+import net.ipv64.kivop.models.PlanAttendance
 import net.ipv64.kivop.services.AuthController
 import net.ipv64.kivop.services.api.ApiConfig.BASE_URL
 import net.ipv64.kivop.services.api.ApiConfig.okHttpClient
-import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.UUID
 
-// Datenmodell für die Antwort
-data class ResponseData(
-    val name: String, // Name der Person
-    val status: Int // Status (z. B. "accepted")
-)
 
-suspend fun responseList(id: String, context: Context): List<ResponseData> =
-    withContext(Dispatchers.IO) {
-      val auth = AuthController(context)
-      val path = "meetings/$id/attendances"
-      
-      val token = auth.getSessionToken()
+suspend fun getAttendances(context: Context, id: String): List<GetAttendanceDTO> = withContext(Dispatchers.IO) {
+  val auth = AuthController(context)
+  val path = "meetings/$id/attendances"
+  
+  val token = auth.getSessionToken()
 
-      if (token.isNullOrEmpty()) {
-        println("Fehler: Kein Token verfügbar")
-        return@withContext emptyList<ResponseData>()
-      }
+  if (token.isNullOrEmpty()) {
+    println("Fehler: Kein Token verfügbar")
+    return@withContext emptyList<GetAttendanceDTO>()
+  }
 
-      val request =
-          Request.Builder().url(BASE_URL + path).addHeader("Authorization", "Bearer $token").get().build()
+  val request =
+      Request.Builder().url(BASE_URL + path).addHeader("Authorization", "Bearer $token").get().build()
 
-      try {
-        val response = okHttpClient.newCall(request).execute()
-        if (response.isSuccessful) {
-          val responseBody = response.body?.string()
-          if (responseBody != null) {
-            // JSON-Antwort parsen
-            val attendancesArray = Gson().fromJson(responseBody, JsonArray::class.java)
-            return@withContext attendancesArray.map { element ->
-              val attendance = element.asJsonObject
-              val identity = attendance.getAsJsonObject("identity")
-              val name = identity.get("name").asString
-              val status = attendance.get("status")?.asString // Kann null sein
-              val status_in =
-                  when (status) {
-                    "accepted" -> 1
-                    "absent" -> 2
-                    else -> 0
-                  }
-              ResponseData(name, status_in) // Relevante Daten extrahieren
-            }
-          } else {
-            println("Fehler: Leere Antwort erhalten.")
-            emptyList<ResponseData>()
+  try {
+    val response = okHttpClient.newCall(request).execute()
+    if (response.isSuccessful) {
+      val responseBody = response.body?.string()
+      if (responseBody != null) {
+        val attendancesArray = Gson().fromJson(responseBody, JsonArray::class.java)
+        return@withContext attendancesArray.map { element ->
+          val attendance = element.asJsonObject
+          val meetingID = attendance.get("meetingId").asString.let { UUID.fromString(it) }
+          val identity = attendance.getAsJsonObject("identity").let { identity ->
+            GetIdentityDTO(
+              identity.get("id").asString.let { UUID.fromString(it) },
+              identity.get("name").asString
+            )
           }
-        } else {
-          println("Fehler bei der Anfrage: ${response.code} - ${response.message}")
-          emptyList<ResponseData>()
+          val status = attendance.get("status")?.asString?.let { AttendanceStatus.valueOf(it) }
+          val itsame = attendance.get("itsame").asBoolean
+          GetAttendanceDTO(
+            meetingID,
+            identity,
+            status,
+            itsame
+          )
         }
-      } catch (e: Exception) {
-        println("Fehler: ${e.message}")
-        emptyList<ResponseData>()
+      } else {
+        println("Fehler: Leere Antwort erhalten.")
+        emptyList()
       }
+    } else {
+      println("Fehler bei der Anfrage: ${response.code} - ${response.message}")
+      emptyList()
     }
+  } catch (e: Exception) {
+    println("Fehler: ${e.message}")
+    emptyList()
+  }
+}
+
+suspend fun planAttendance(context: Context,meetingId: String, status: PlanAttendance):Boolean = withContext(Dispatchers.IO) {
+  var auth = AuthController(context)
+  val path = "/meetings/$meetingId/plan-attendance/${status.name}"
+
+  val token = auth.getSessionToken()
+
+
+  if (token.isNullOrEmpty()) {
+    println("Fehler: Kein Token verfügbar")
+    return@withContext false
+  }
+
+  val emptyBody = ByteArray(0).toRequestBody(null)
+
+  val request =
+    Request.Builder()
+      .url(BASE_URL + path)
+      .addHeader("Authorization", "Bearer $token")
+      .put(emptyBody)
+      .build()
+
+  return@withContext try {
+    val response = okHttpClient.newCall(request).execute()
+    if (response.isSuccessful) {
+      true
+    } else {
+      println("Fehler bei der Anfrage: ${response.message}")
+      false
+    }
+  } catch (e: Exception) {
+    Log.e("put", "Fehler: ${e.message}")
+    false
+  }
+}
