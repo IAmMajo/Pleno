e6de4e84abb79401fbdfb280e8fa92b9f874dcff
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-19 04:21:21 +0100

    Removed unsued imports - #248

--- android/app/src/main/java/net/ipv64/kivop/services/api/MeetingsApi.kt
+++ android/app/src/main/java/net/ipv64/kivop/services/api/MeetingsApi.kt
@@ -1,15 +1,10 @@
 package net.ipv64.kivop.services.api
 
 import android.content.Context
-import android.util.Log
-import com.example.kivopandriod.moduls.Location
-import com.example.kivopandriod.moduls.SitzungsCardData
 import com.example.kivopandriod.services.stringToLocalDateTime
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.AttendanceStatus
@@ -17,8 +12,6 @@ import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetIdentityDTO
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetLocationDTO
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetMeetingDTO
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.MeetingStatus
-import net.ipv64.kivop.moduls.AttendancesListsData
-import net.ipv64.kivop.moduls.GetVotingOptionDTO
 import net.ipv64.kivop.services.AuthController
 import net.ipv64.kivop.services.api.ApiConfig.BASE_URL
 import net.ipv64.kivop.services.api.ApiConfig.okHttpClient
