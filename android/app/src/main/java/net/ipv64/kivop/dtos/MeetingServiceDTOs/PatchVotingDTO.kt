7aa0a522f88fc93912344844a2dc4f29e342d752
KIVoP Debian Gitlab Runner <lennart-luis.guggenberger@hsrw.org>
2024-12-19 18:56:57 +0100

    Format

--- android/app/src/main/java/net/ipv64/kivop/dtos/MeetingServiceDTOs/PatchVotingDTO.kt
+++ android/app/src/main/java/net/ipv64/kivop/dtos/MeetingServiceDTOs/PatchVotingDTO.kt
@@ -4,8 +4,6 @@ data class PatchVotingDTO(
     var question: String?,
     var description: String?,
     var anonymous: Boolean?,
-    var options:
-        List<
-            GetVotingOptionDTO>?, // If options is not nil: ALL previous options will be
-                                  // deleted/replaced
+    var options: List<GetVotingOptionDTO>?, // If options is not nil: ALL previous options will be
+    // deleted/replaced
 )
