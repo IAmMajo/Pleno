a1d30bb682f6064dd4c1bf3f3120324021021b3f
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-20 01:38:03 +0100

    Resolved merge conflict in UpdatePosterPositionDTO.kt

--- android/app/src/main/java/net/ipv64/kivop/dtos/PosterServiceDTOs/UpdatePosterPositionDTO.kt
+++ android/app/src/main/java/net/ipv64/kivop/dtos/PosterServiceDTOs/UpdatePosterPositionDTO.kt
@@ -1,12 +1,15 @@
 package net.ipv64.kivop.dtos.PosterServiceDTOs
 
+
+
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class UpdatePosterPositionDTO (
-    var posterId : UUID?,
-    var latitude : Double?,
-    var longitude : Double?,
-    var expires_at : LocalDateTime?,
-    var responsible_users : List<UUID>?,
-    var image : ByteArray?,
+  var posterId : UUID?,
+  var latitude : Double?,
+  var longitude : Double?,
+  var expires_at : LocalDateTime?,
+  var responsible_users : List<UUID>?,
+  var image : ByteArray?,
 )
