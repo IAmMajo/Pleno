7ceb9d5fdd29fcf4916f4a635d26c613ca0db3c3
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-21 07:08:43 +0100

    added customRoundedBottom

--- android/app/src/main/java/net/ipv64/kivop/ui/customRoundedSide.kt
+++ android/app/src/main/java/net/ipv64/kivop/ui/customRoundedSide.kt
@@ -1,25 +1,15 @@
 package net.ipv64.kivop.ui
 
-import android.R.attr.path
-import android.util.Log
 import androidx.annotation.IntRange
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.DrawResult
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-
-import net.ipv64.kivop.ui.theme.Primary
 
 
 //// Absolute values version
@@ -68,7 +58,7 @@ fun Modifier.customRoundedTop(
   height: Float = 50f,
   width: Float = 35f
 ): Modifier = drawBehind {
-  applyCustomRoundedTop(color, height, width)
+  applyCustomRoundedTop(color, -height, width)
 }
 
 // Percentage values version
@@ -80,24 +70,50 @@ fun Modifier.customRoundedTop(
 ): Modifier = drawBehind  {
   val height = maxHeight * (heightPercent / 100f)
   val width = size.width * (widthPercent / 100f)
-  applyCustomRoundedTop(color, height, width)
+  applyCustomRoundedTop(color, -height, width,)
 }
 
+fun Modifier.customRoundedBottom(
+  color: Color = Color.Blue,
+  height: Float = 50f,
+  width: Float = 35f
+): Modifier = drawBehind {
+  
+  applyCustomRoundedTop(color, height, width,size.height)
+}
+
+// Percentage values version
+
+fun Modifier.customRoundedBottom(
+  color: Color = Color.Blue,
+  @IntRange(from = 0, to = 100) heightPercent: Int = 0,
+  @IntRange(from = 0, to = 50) widthPercent: Int = 0,
+  maxHeight: Float = 400f,
+): Modifier = drawBehind  {
+  val height = maxHeight * (heightPercent / 100f)
+  val width = size.width * (widthPercent / 100f)
+  applyCustomRoundedTop(color, height, width,size.height)
+}
+
+  
+
 // Shared implementation logic
-private fun DrawScope.applyCustomRoundedTop(color: Color, height: Float, width: Float) {
+private fun DrawScope.applyCustomRoundedTop(color: Color, height: Float, width: Float,start: Float = 0f) {
   val widthLimited = width.coerceAtMost(size.width / 2)
 
   // Define control points for the cubic Bézier curve
-  val point1 = Offset(widthLimited, -height)
-  val point2 = Offset(size.width - widthLimited, -height)
+  val point1 = Offset(width, height+start)
+  val point2 = Offset(size.width - widthLimited, height+start)
 
   // Create the path for the rounded top
   val path = Path().apply {
-    moveTo(0f, 0f)
-    cubicTo(point1.x, point1.y, point2.x, point2.y, size.width, 0f)
-    lineTo(size.width, size.height)
-    lineTo(0f, size.height)
+    moveTo(0f, start)
+    cubicTo(point1.x, point1.y, point2.x, point2.y, size.width, start)
+    //lineTo(size.width, size.height)
+    //lineTo(0f, size.height)
     close()
   }
-    drawPath(path, color)
+  //debug bezier curve points
+  //drawPoints(listOf(point1,point2), pointMode = PointMode.Points,Color.Cyan,10f)
+  drawPath(path, color)
 }
