dae1436e9b0514e45e1c8316b04881582cc0db41
Roman, Evgeny <evgeny.roman@hsrw.org>
2024-12-24 03:15:22 +0100

    added common spacers

---
+++ android/app/src/main/java/net/ipv64/kivop/components/SpacerBetweenElements.kt
@@ -0,0 +1,12 @@
+package net.ipv64.kivop.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerBetweenElements(){
+  Spacer(modifier = Modifier.height(16.dp))
+}
