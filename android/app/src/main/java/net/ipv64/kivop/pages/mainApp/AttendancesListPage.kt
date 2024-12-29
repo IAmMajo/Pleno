0fdce0268b70e334ad5b1cc2764ae25c6a6fede9
Gehrke, Justin <justin.gehrke@hsrw.org>
2024-12-29 01:49:57 +0100

    update: Update AttendancesListPage to integrate CustomIndicator
    - Updated `AttendancesListPage` to align with the new `CustomIndicator`.
    - Positioned in-session meetings above the tabs (requires further adjustments).
    
    Refs: @30226, #277

--- android/app/src/main/java/net/ipv64/kivop/pages/mainApp/AttendancesListPage.kt
+++ android/app/src/main/java/net/ipv64/kivop/pages/mainApp/AttendancesListPage.kt
@@ -1,6 +1,14 @@
 package net.ipv64.kivop.pages.mainApp
 
-import AppointmentTabContent
+import GenerateTabs
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -8,22 +16,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
+import net.ipv64.kivop.components.ListenItem
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetMeetingDTO
+import net.ipv64.kivop.dtos.MeetingServiceDTOs.MeetingStatus
 import net.ipv64.kivop.services.api.getMeetings
 
 @Composable
 fun AttendancesListPage(navController: NavController) {
-  val tabs = listOf("anstehende Sitzungen", "vergangenen Sitzungen")
+  val tabs = listOf("Anstehende Sitzungen", "Vergangene Sitzungen")
   val scope = rememberCoroutineScope()
   var meetings by remember { mutableStateOf<List<GetMeetingDTO>>(emptyList()) }
 
-  //    val colorH: Color = when (STATUS) {
-  //        0 -> Background_secondary_light
-  //        1 -> Primary_dark_20
-  //        else -> Error_dark_20
-  //    }
+
 
   LaunchedEffect(Unit) {
     scope.launch {
@@ -34,8 +42,60 @@ fun AttendancesListPage(navController: NavController) {
       meetings = result
     }
   }
-  
-  // Log.i("AttendancesListPage", "Appointments: ${appointments}")
-  // Ergebnisse anzeigen
-  AppointmentTabContent(navController, tabs = tabs, appointments = meetings)
+
+
+  val scheduledMeetings = meetings.filter { it.status  == MeetingStatus.scheduled}
+  val completedMeetings = meetings.filter { it.status  == MeetingStatus.completed}
+  val inSessionMeetings = meetings.filter { it.status  == MeetingStatus.inSession}
+
+  // Tab-Inhalte definieren
+  val tabContents = listOf<@Composable () -> Unit>(
+    {
+      LazyColumn(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+      ) {
+        items(scheduledMeetings) { meeting ->
+          ListenItem(itemListData = meeting, onClick = {
+            navController.navigate("anwesenheit/${meeting.id}")
+          })
+        }
+      }
+    },
+    {
+      LazyColumn(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+      ) {
+        items(completedMeetings) { meeting ->
+          ListenItem(itemListData = meeting, onClick = {
+            navController.navigate("anwesenheit/${meeting.id}")
+          })
+        }
+      }
+    }
+  )
+
+ 
+  // Layout mit Tabs und aktuellen Meetings
+  Column {
+    // Aktuelle Meetings anzeigen
+    LazyColumn(
+      modifier = Modifier.heightIn(max = 210.dp),
+      verticalArrangement = Arrangement.spacedBy(12.dp)
+      
+    ) {
+      items(inSessionMeetings) { meeting ->
+        ListenItem(itemListData = meeting, onClick = {
+          navController.navigate("anwesenheit/${meeting.id}")
+        })
+      }
+    }
+    Spacer(Modifier.size(18.dp))
+    // Tabs anzeigen
+    GenerateTabs(
+      tabs = tabs,
+      tabContents = tabContents
+    )
+  }
 }
