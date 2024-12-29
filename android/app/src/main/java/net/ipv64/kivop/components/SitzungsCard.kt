5409a5208f75fec4cff834022b15c68f94487896
Gehrke, Justin <justin.gehrke@hsrw.org>
2024-12-29 05:42:11 +0100

    update: SitzungsCard.kt
    - Updated `SitzungsCard.kt` to match the new design:
      - Added support for a customizable background color.
      - Address is now displayed on a separate line as it no longer fits with other values.
    
    Refs: @30226, #281

--- android/app/src/main/java/net/ipv64/kivop/components/SitzungsCard.kt
+++ android/app/src/main/java/net/ipv64/kivop/components/SitzungsCard.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,10 +23,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.UUID
 import net.ipv64.kivop.R
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.AttendanceStatus
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetIdentityDTO
@@ -36,165 +30,142 @@ import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetLocationDTO
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.GetMeetingDTO
 import net.ipv64.kivop.dtos.MeetingServiceDTOs.MeetingStatus
 import net.ipv64.kivop.ui.theme.Background_secondary
-import net.ipv64.kivop.ui.theme.Text_prime
+import net.ipv64.kivop.ui.theme.Text_prime_light
+import java.time.LocalDateTime
+import java.util.UUID
+
 
 @Composable
-fun SitzungsCard(GetMeetingDTO: GetMeetingDTO) {
-  val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-  val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+fun SitzungsCard(GetMeetingDTO: GetMeetingDTO, backgroundColor: Color = Color.Transparent) {
+  val dateFormatter = java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy")
+  val timeFormatter = java.time.format.DateTimeFormatter.ofPattern("HH:mm")
 
-  // Kartenlayout
   Box(
-      modifier =
-          Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)).background(Color(0xFFBFEFB1))) {
-        Column(modifier = Modifier.padding(12.dp).background(Color.Transparent)) {
-          Text(
-              text = GetMeetingDTO.name,
-              color = Text_prime,
-              style = MaterialTheme.typography.titleLarge,
-              fontWeight = FontWeight.Bold)
-          Spacer(modifier = Modifier.height(8.dp))
-          Row {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-              Icon(
-                  tint = Text_prime,
-                  painter = painterResource(id = R.drawable.ic_calendar),
-                  contentDescription = "Icon Kalender")
-              Spacer(modifier = Modifier.width(4.dp))
-              Text(
-                  text = GetMeetingDTO.start.format(dateFormatter),
-                  color = Text_prime,
-                  style = MaterialTheme.typography.bodyMedium,
-                  fontWeight = FontWeight.Bold)
-            }
-
-            Spacer(modifier = Modifier.width(4.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-              Icon(
-                  painter = painterResource(id = R.drawable.ic_clock),
-                  tint = Text_prime,
-                  contentDescription = "Icon Uhrzeit")
-              Spacer(modifier = Modifier.width(4.dp))
-              Text(
-                  text =
-                      "${GetMeetingDTO.start.format(timeFormatter)} Uhr (${GetMeetingDTO.duration} Min.)",
-                  color = Text_prime,
-                  style = MaterialTheme.typography.bodyMedium,
-                  fontWeight = FontWeight.Bold)
-            }
-          }
-          Spacer(modifier = Modifier.height(8.dp))
-          Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            // Icon für Standort
-            Icon(
-                painter = painterResource(id = R.drawable.ic_place),
-                tint = Text_prime,
-                contentDescription = "Icon Standort",
-                modifier = Modifier.padding(end = 8.dp) // Abstand zwischen Icon und Text
-                )
-            // Text-Darstellung
-            Column {
-              // Name der Location
-              Text(
-                  text =
-                      "${GetMeetingDTO.location?.name}${if (!GetMeetingDTO.location?.street.isNullOrBlank()) "," else ""}",
-                  color = Text_prime,
-                  style = MaterialTheme.typography.bodyMedium,
-                  fontWeight = FontWeight.Bold)
-              // Adresse der Location
-              if (!GetMeetingDTO.location?.street.isNullOrBlank())
-                  Text(
-                      text =
-                          buildString {
-                            // Füge die Straße hinzu
-                            append(GetMeetingDTO.location?.street ?: "")
-
-                            // Füge "Str." und die Nummer hinzu, falls die Nummer nicht null ist
-                            if (!GetMeetingDTO.location?.number.isNullOrBlank()) {
-                              Log.d(
-                                  "Test-zu",
-                                  "AttendancesCoordinationPage:${GetMeetingDTO.location?.street!!?.length} <--")
-                              append(" Str. ${GetMeetingDTO.location?.number}")
-                            }
-
-                            // Füge ein Komma hinzu, falls postalCode oder place nicht null sind
-                            if (!GetMeetingDTO.location?.postalCode.isNullOrBlank() ||
-                                !GetMeetingDTO.location?.place.isNullOrBlank()) {
-                              append(", ")
-                            }
-
-                            // Füge postalCode und place hinzu
-                            append(GetMeetingDTO.location?.postalCode ?: "")
-                            append(" ${GetMeetingDTO.location?.place ?: ""}")
-                          },
-                      color = Text_prime,
-                      style = MaterialTheme.typography.bodyMedium,
-                      fontWeight = FontWeight.Bold)
-            }
-          }
-
-          Spacer(modifier = Modifier.height(8.dp))
-          ProfilCardKlein()
-          Spacer(modifier = Modifier.height(4.dp))
-        }
-      }
-}
-
-@Composable
-fun ProfilCardKlein() {
-  val name = "T"
-  Row() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.size(40.dp).clip(CircleShape).background(Background_secondary)) {
-          // Buchstabe in Box (round)
-          Text(
-              text = name,
-              color = Text_prime,
-              fontSize = 18.sp,
-              style = MaterialTheme.typography.bodyLarge,
-              fontWeight = FontWeight.Bold)
-        }
-    Spacer(modifier = Modifier.width(4.dp))
-    Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+    modifier = Modifier
+      .fillMaxWidth()
+      .clip(RoundedCornerShape(16.dp))
+      .background(backgroundColor)
+      .padding(16.dp)
+  ) {
+    Column {
       Text(
-          text = "Thorsten Teebeutel",
-          color = Text_prime,
-          // fontSize = 18.sp,
-          style = MaterialTheme.typography.bodyMedium,
-          fontWeight = FontWeight.Bold)
-      Text(
-          text = "Sitzungsleiter", // ToDo - Rolle anpassen
-          color = Text_prime,
-          fontSize = 12.sp,
+        text = GetMeetingDTO.name,
+        color = Color.White,
+        style = MaterialTheme.typography.headlineMedium,
+        fontWeight = FontWeight.Bold
       )
+      Spacer(modifier = Modifier.height(12.dp))
+      Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+        Icon(
+          painter = painterResource(id = R.drawable.ic_calendar),
+          contentDescription = "Datum",
+          tint = Text_prime_light
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+          text = "${GetMeetingDTO.start.format(dateFormatter)} | ${GetMeetingDTO.start.format(timeFormatter)} Uhr",
+          color = Text_prime_light,
+          style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Icon(
+          painter = painterResource(id = R.drawable.ic_clock),
+          contentDescription = "Dauer",
+          tint = Text_prime_light
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+          text = "${GetMeetingDTO.duration} Min",
+          color = Text_prime_light,
+          style = MaterialTheme.typography.bodyLarge
+          
+        )
+      }
+      Spacer(modifier = Modifier.height(8.dp))
+      Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        // Icon für Standort
+        Icon(
+          painter = painterResource(id = R.drawable.ic_place),
+          tint = Background_secondary,
+          contentDescription = "Icon Standort",
+          modifier = Modifier.padding(end = 8.dp) // Abstand zwischen Icon und Text
+        )
+        // Text-Darstellung
+ 
+          // Name der Location
+          Text(
+            text =
+            "${GetMeetingDTO.location?.name}${if (!GetMeetingDTO.location?.street.isNullOrBlank()) ", " else ""}",
+            color = Text_prime_light,
+            style = MaterialTheme.typography.bodyMedium)
+          // Adresse der Location
+          if (!GetMeetingDTO.location?.street.isNullOrBlank())
+            Text(
+              text =
+              buildString {
+                // Füge die Straße hinzu
+                append(GetMeetingDTO.location?.street ?: "")
+
+                // Füge "Str." und die Nummer hinzu, falls die Nummer nicht null ist
+                if (!GetMeetingDTO.location?.number.isNullOrBlank()) {
+                  Log.d(
+                    "Test-zu",
+                    "AttendancesCoordinationPage:${GetMeetingDTO.location?.street!!?.length} <--")
+                  append(" Str. ${GetMeetingDTO.location?.number}")
+                }
+
+                // Füge ein Komma hinzu, falls postalCode oder place nicht null sind
+                if (!GetMeetingDTO.location?.postalCode.isNullOrBlank() ||
+                  !GetMeetingDTO.location?.place.isNullOrBlank()) {
+                  append(", ")
+                }
+
+                // Füge postalCode und place hinzu
+                append(GetMeetingDTO.location?.postalCode ?: "")
+                append(" ${GetMeetingDTO.location?.place ?: ""}")
+              },
+              color = Text_prime_light,
+              style = MaterialTheme.typography.bodyMedium,
+            )
+        
+      }
+
+      Spacer(modifier = Modifier.height(16.dp))
+      ProfileCardSmall(name = GetMeetingDTO.chair?.name ?: "", role = "Sitzungsleiter", profilePicture = null,backgroundColor = Background_secondary.copy(0.15f),texColor = Text_prime_light)
     }
   }
 }
 
+
+
+
+
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun SitzungsCardPreview() {
-  val location =
-      GetLocationDTO(
-          letter = "A",
-          street = "keineanung",
-          name = "Gemeindesaal",
-          id = "sd".let { UUID.fromString(it) },
-          number = "",
-          place = "Kam",
-          postalCode = "2425")
-  val test =
-      GetMeetingDTO(
-          name = "Jahreshauptversammlung",
-          start = LocalDateTime.now(),
-          id = "123".let { UUID.fromString(it) },
-          duration = 90.toUShort(),
-          location = location,
-          description = "",
-          status = MeetingStatus.completed,
-          chair = GetIdentityDTO("123".let { UUID.fromString(it) }, "Thorsten Teebeutel"),
-          code = "23",
-          myAttendanceStatus = AttendanceStatus.present)
+  val location = GetLocationDTO(
+    letter = "A",
+    street = "keineanung",
+    name = "Gemeindesaal",
+    id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),
+    number = "4",
+    place = "Kam",
+    postalCode = "2425"
+  )
+  val test = GetMeetingDTO(
+    name = "Jahreshauptversammlung",
+    start = LocalDateTime.of(2023, 12, 31, 10, 0),
+    id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),
+    duration = 90.toUShort(),
+    location = location,
+    description = "",
+    status = MeetingStatus.completed,
+    chair = GetIdentityDTO(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), "Thorsten Teebeutel"),
+    code = "23",
+    myAttendanceStatus = AttendanceStatus.present
+  )
   SitzungsCard(GetMeetingDTO = test)
 }
