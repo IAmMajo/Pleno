d48f8cc0211ea7b19c5c17089db3bdf58e10825f
maxi10807 <maximilianeinhaus@gmail.com>
2024-12-29 18:01:01 +0100

    Passwort reset/change - #206

--- backend/auth-service/Sources/App/DTOs/UserEmailVerificationDTO.swift
+++ backend/auth-service/Sources/App/DTOs/UserEmailVerificationDTO.swift
@@ -1,8 +1,6 @@
 import Fluent
 import Models
 import Fluent
-
-import Fluent
 import Vapor
 
 public struct UserEmailVerificationDTO: Content, Sendable {
@@ -13,7 +11,7 @@ public struct UserEmailVerificationDTO: Content, Sendable {
     public var createdAt: Date?
 }
 
-public enum  VerificationStatus: String, Codable {
+public enum  VerificationStatus: String, Codable, Sendable {
     case failed
     case pending
     case verified
