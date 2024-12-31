7b88d55dc8aef7b533a7ae157f688bc65eeee35d
kiwiscodeplanet <alinahartmann@gmx.net>
2024-12-31 16:22:45 +0100

    #59 changed Buttons

--- android/app/src/main/java/net/ipv64/kivop/components/PopupCallToConfirmation.kt
+++ android/app/src/main/java/net/ipv64/kivop/components/PopupCallToConfirmation.kt
@@ -1,23 +1,20 @@
 package com.example.kivopandriod.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.kivopandriod.R
-import com.example.kivopandriod.ui.theme.Background_light
-import com.example.kivopandriod.ui.theme.Primary_dark
-import com.example.kivopandriod.ui.theme.Primary_dark_20
-import com.example.kivopandriod.ui.theme.Text_light
+import net.ipv64.kivop.ui.theme.BG_Pri
+import net.ipv64.kivop.ui.theme.Secondary
+import net.ipv64.kivop.ui.theme.Tertiary
+import net.ipv64.kivop.ui.theme.Text_Ter
 
 
 @Composable
@@ -27,81 +24,57 @@ fun CallToConfirmation(
   dialogTitle: String,
   dialogText: String,
   buttonOneText: String,
-  buttonTextColorConfirm: Color, // (+rename)
   buttonTwoText: String?,
-  buttonOneColor: Color,
-  buttonTwoColor: Color?,
-  buttonColorDismiss: Color,
   buttonTextDismiss: String,
-  buttonTextColorDismiss: Color,
+  alert: Boolean,
 
-
-  // iconIdTop: Int,
 ){
-  //val buttonOneColor = Color.Cyan
-
-
   AlertDialog(
     onDismissRequest = onDismissRequest ,
     confirmButton = {
       // Button-One
       Button(
         onClick = onConfirmation,
-        // modifier: Modifier = Modifier,
+        modifier = Modifier
+          .height(35.dp),
         enabled = true,
         shape = ButtonDefaults.textShape,
-        colors = ButtonDefaults.buttonColors(buttonOneColor),
+        colors = ButtonDefaults.buttonColors(Tertiary),
         elevation = null,
         border =  null,
-        contentPadding = PaddingValues(horizontal = 15.dp, vertical = 8.dp), //ToDo - PaddingValues - anpassen?
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp), //ToDo - PaddingValues - anpassen?
         //interactionSource = remember { MutableInteractionSource() }, //MutableInteractionSource
         // content: @Composable() (RowScope.() -> Unit)
       ){
-        Text( text = buttonOneText,
-          color = buttonTextColorConfirm )
+        Text( 
+          text = buttonOneText,
+          color = BG_Pri,
+          modifier = Modifier
+            .padding(horizontal = 12.dp))
       }
-      // Button-Two
-//            if (buttonTwoText != null) {
-//                Button(
-//                    onClick = onConfirmation,
-//                    // modifier: Modifier = Modifier,
-//                    enabled = true,
-//                    shape = RoundedCornerShape(8.dp),
-//                    colors = ButtonDefaults.buttonColors(if (buttonTwoColor != null) buttonTwoColor else {Color.Transparent}),
-//                    elevation = null,
-//                    border = null,
-//                    contentPadding = PaddingValues(horizontal = 15.dp, vertical = 8.dp), //ToDo - PaddingValues - anpassen
-//                    //interactionSource = remember { MutableInteractionSource() }, //MutableInteractionSource
-//                    // content: @Composable() (RowScope.() -> Unit)
-//                ) {
-//                    Text(text = buttonTwoText)
-//                }
-//            }
     },
     // Dismiss-Button, TODO - fix placing
     dismissButton = {
       Button(
         onClick = onDismissRequest,
-        // modifier: Modifier = Modifier,
+        modifier = Modifier
+          .height(35.dp),
         enabled = true,
         shape = ButtonDefaults.textShape,
-        colors = ButtonDefaults.buttonColors(buttonColorDismiss),
+        colors = ButtonDefaults.buttonColors(Secondary),
         elevation = null,
         border =  null,
         contentPadding = ButtonDefaults.TextButtonContentPadding, //ToDo - PaddingValues - anpassen?
         //interactionSource = remember { MutableInteractionSource() }, //MutableInteractionSource
         //content: @Composable() (RowScope.() -> Unit)
       ){
-//                if () {
-//                    Icon(
-//                        tint = Text_light,
-//                        painter = painterResource(id = R.drawable.ic_cancel),
-//                        contentDescription = "Icon Cancel"
-//                    )
-//                }
+//    
         Text(
           text = buttonTextDismiss,
-          color = buttonTextColorDismiss)
+          color = Text_Ter,
+          modifier = Modifier
+            .padding(horizontal = 12.dp)
+        )
       }
     },
 //        icon = {
@@ -135,16 +108,15 @@ fun Screen(){
     onDismissRequest = close,
     onConfirmation = openCam,
     dialogTitle = "TestTitel",
-    dialogText = "Test Text/ Beschreibung",
+    dialogText = "Sind Sie sicher, dass SIe Ihr Ergebnis abschickenmöchten?",
     buttonOneText = "Bestätigen",
-    buttonTextColorConfirm = Background_light,
+   // buttonTextColorConfirm = Background_light,
     buttonTwoText = "Two Button",
-    buttonOneColor = Primary_dark,
-    buttonTwoColor = Primary_dark,
-    buttonColorDismiss = Primary_dark_20,
+  //  buttonOneColor = Primary_dark,
+   // buttonTwoColor = Primary_dark,
+    // buttonColorDismiss = Primary_dark_20,
     buttonTextDismiss = "Abbrechen",
-    buttonTextColorDismiss = Text_light,
-    // iconIdTop =  R.drawable.ic_open
+    alert = false
   )
 }
 
--- android/app/src/main/java/net/ipv64/kivop/ui/theme/Color.kt
+++ android/app/src/main/java/net/ipv64/kivop/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package net.ipv64.kivop.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import org.w3c.dom.Text
 
 val Primary_dark = Color(0xFF2EA100)
 val Primary_dark_20 = Color(0x332EA100)
@@ -12,3 +13,17 @@ val Text_light = Color(0xFF1A1C15)
 val Error_dark = Color(0xFFFF5449)
 val Error_dark_20 = Color(0x33FF5449)
 val Secondary_dark = Color(0xFFA7D293)
+
+//light 
+val Primary = Color(0xff2351c4)
+val Secondary = Color(0xff9ab1eb)
+val Tertiary = Color(0xff1061da)
+val Tertiary_60 = Color(0x991061da)
+val Text_Pri = Color(0xff373434)
+val Text_Sec = Color(0xff43578a)
+val Text_Ter = Color(0xff404445)
+val BG_Pri = Color(0xfff5f6f8)
+val BG_Sec = Color(0xfff7fbfc)
+val BG_Ter = Color(0xffcdeaf7)
+val Neutral_Gray = Color(0xff686d74)
+val Deny = Color(0xffda1043)
