18f032b10256d06cb6543e2cbf9382a46256a4b8
Adrian Paus <adrian.paus@hsrw.org>
2025-01-07 20:23:20 +0100

    RideServiceMainView hinzugefügt

---
+++ kivopadmin/kivopadmin/Views/RideService/RideServiceMainView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+//import RideServiceDTOs
+
+struct RideServiceMainView: View{
+    var body: some View {
+        NavigationStack{
+            Text("Hallo")
+                .navigationTitle("Fahrgemeinschaften")
+        }
+    }
+}
+
+#Preview {
+    RideServiceMainView()
+}
