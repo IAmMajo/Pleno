bcedd894bd2e72246bdb9996c9604724361b53c4
Roman, Evgeny <evgeny.roman@hsrw.org>
2025-01-08 00:21:47 +0100

    Removed local changes from DTO

--- android/app/src/main/java/net/ipv64/kivop/dtos/AuthServiceDTOs/UserRegistrationDTO.kt
+++ android/app/src/main/java/net/ipv64/kivop/dtos/AuthServiceDTOs/UserRegistrationDTO.kt
@@ -1,8 +1,11 @@
 package net.ipv64.kivop.dtos.AuthServiceDTOs
 
-data class UserRegistrationDTO(
-    var name: String?,
-    var email: String?,
-    var password: String?,
-    var profileImage: String?,
+import java.util.UUID
+import java.time.LocalDateTime
+
+data class UserRegistrationDTO (
+  var name : String?,
+  var email : String?,
+  var password : String?,
+  var profileImage : String?,
 )
