915514a542b3bd881c4034b465ed2f2dbada4aee
Tunnissen, Jan <jan.tunnissen@hsrw.org>
2025-01-11 15:12:12 +0100

    config-service wieder aktiviert - #318

--- backend/ride-service/Sources/App/configure.swift
+++ backend/ride-service/Sources/App/configure.swift
@@ -20,19 +20,19 @@ public func configure(_ app: Application) async throws {
     ), as: .psql)
 
     // Settings beim Start laden
-//    Task {
-//        do {
-//            let configServiceURL = Environment.get("CONFIG_SERVICE_URL") ?? "http://kivop-config-service"
-//            let serviceIDString = Environment.get("SERVICE_ID") ?? "d355c12b-19c6-4f67-b5b5-837030ed09e6"
-//            guard let serviceID = UUID(uuidString: serviceIDString) else {
-//                app.logger.error("Ungültige Service-ID.")
-//                return
-//            }
-//            try await SettingsManager.shared.loadSettings(from: configServiceURL, serviceID: serviceID, client: app.client, logger: app.logger)
-//        } catch {
-//            app.logger.error("Fehler beim Laden der Einstellungen: \(error.localizedDescription)")
-//        }
-//    }
+    Task {
+        do {
+            let configServiceURL = Environment.get("CONFIG_SERVICE_URL") ?? "http://kivop-config-service"
+            let serviceIDString = Environment.get("SERVICE_ID") ?? "d355c12b-19c6-4f67-b5b5-837030ed09e6"
+            guard let serviceID = UUID(uuidString: serviceIDString) else {
+                app.logger.error("Ungültige Service-ID.")
+                return
+            }
+            try await SettingsManager.shared.loadSettings(from: configServiceURL, serviceID: serviceID, client: app.client, logger: app.logger)
+        } catch {
+            app.logger.error("Fehler beim Laden der Einstellungen: \(error.localizedDescription)")
+        }
+    }
     
     app.migrations.add(CreatePlenoEvent())
     app.migrations.add(CreateEventParticipant())
