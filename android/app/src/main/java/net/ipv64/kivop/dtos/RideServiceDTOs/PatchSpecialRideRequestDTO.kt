8a53082e282bc01f9d1246cf7eb1a3b5075b57a5
KIVoP Debian Gitlab Runner <lennart-luis.guggenberger@hsrw.org>
2025-01-11 11:48:27 +0100

    Kotlin-DTOs automatisch generiert

--- android/app/src/main/java/net/ipv64/kivop/dtos/RideServiceDTOs/PatchSpecialRideRequestDTO.kt
+++ android/app/src/main/java/net/ipv64/kivop/dtos/RideServiceDTOs/PatchSpecialRideRequestDTO.kt
@@ -3,9 +3,8 @@ package net.ipv64.kivop.dtos.RideServiceDTOs
 import java.util.UUID
 import java.time.LocalDateTime
 
-data class PatchParticipationDTO (
-    var driver : Boolean?,
-    var passengers_count : Int?,
+data class PatchSpecialRideRequestDTO (
     var latitude : Float?,
     var longitude : Float?,
+    var accepted : Boolean?,
 )
