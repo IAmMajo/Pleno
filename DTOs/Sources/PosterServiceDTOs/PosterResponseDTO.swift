c64840ca1780c4971022ed5d2b7c31fd7f6f2e43
Dennis Sept <31742>
2025-01-18 22:49:49 +0300

    imageUrl zu image

--- DTOs/Sources/PosterServiceDTOs/PosterResponseDTO.swift
+++ DTOs/Sources/PosterServiceDTOs/PosterResponseDTO.swift
@@ -4,13 +4,13 @@ public struct PosterResponseDTO: Codable {
     public var id: UUID
     public var name: String
     public var description: String?
-    public var imageUrl: String
+    public var image: Data
    
-    public init(id: UUID, name: String, description: String? = nil, imageUrl: String) {
+    public init(id: UUID, name: String, description: String? = nil, image: Data) {
         self.id = id
         self.name = name
         self.description = description
-        self.imageUrl = imageUrl
+        self.image = image
        
     }
 }
