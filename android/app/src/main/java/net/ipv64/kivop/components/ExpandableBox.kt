3bcf345e731f2304e06016b4536d716e60382324
Gehrke, Justin <justin.gehrke@hsrw.org>
2025-01-19 14:56:42 +0100

    add: components/ExpandableBox.kt, models/attendancesList.kt, services/GetScreenHeight.kt
    - **ExpandableBox.kt**:
      - Accepts two `@Composable` parameters:
        - `contentFoldedIn`: Represents the collapsed state.
        - `contentFoldedOut`: Represents the expanded state.
    
    - **attendancesList.kt**: Replaced `ResponseItem` with `attendancesList.kt`.
    
    - **GetScreenHeight.kt**: Returns the height of the screen.
    
    Refs: @30226, #343

---
+++ android/app/src/main/java/net/ipv64/kivop/components/ExpandableBox.kt
@@ -0,0 +1,58 @@
+package net.ipv64.kivop.components
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
+
+@Composable
+fun ExpandableBox(contentFoldedIn: @Composable () -> Unit,contentFoldedOut: @Composable () -> Unit) {
+
+
+  var expanded by remember { mutableStateOf(false) }
+  Surface(
+    modifier = Modifier.fillMaxWidth(),
+    color = Color.Transparent,
+    onClick = { expanded = !expanded },
+  ) {
+    AnimatedContent(
+      targetState = expanded,
+      transitionSpec = {
+        fadeIn(animationSpec = tween(150, 150)) togetherWith
+          fadeOut(animationSpec = tween(150)) using
+          SizeTransform { initialSize, targetSize ->
+            keyframes {
+              // Expand horizontally first.
+              IntSize(targetSize.width, initialSize.height) at 800
+              durationMillis = 300
+            }
+          }
+      }, label = "size transform"
+    ) { targetExpanded ->
+      if (targetExpanded) {
+
+        contentFoldedOut()
+        
+      }
+      else {
+
+        contentFoldedIn()
+        
+      }
+    }
+   
+  }
+}
