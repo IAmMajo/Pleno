73bb1027899ad1d12ee99954428488c600456c47
Dennis Sept <31742>
2025-01-21 17:31:56 +0300

    dtos angepasst

--- DTOs/Sources/PosterServiceDTOs/HangPosterPositionDTO.swift
+++ DTOs/Sources/PosterServiceDTOs/HangPosterPositionDTO.swift
@@ -2,8 +2,12 @@ import Foundation
 
 public struct HangPosterPositionDTO: Codable {
     public var image: Data
+    public var latitude: Double?
+    public var longitude: Double?
     
-    public init(image: Data) {
+    public init(image: Data, latitude: Double? = nil, longitude: Double? = nil) {
         self.image = image
+        self.latitude = latitude
+        self.longitude = longitude
     }
 }
