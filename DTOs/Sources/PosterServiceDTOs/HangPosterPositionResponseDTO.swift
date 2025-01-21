a421ab1d69b8bb893a16af77aa88e958cbb46084
Dennis Sept <31742>
2025-01-21 17:38:02 +0300

    Poster-Service long und lat felder den hangsDtos hinzugefügt

--- DTOs/Sources/PosterServiceDTOs/HangPosterPositionResponseDTO.swift
+++ DTOs/Sources/PosterServiceDTOs/HangPosterPositionResponseDTO.swift
@@ -4,12 +4,16 @@ public struct HangPosterPositionResponseDTO: Codable {
     public var posterPosition: UUID
     public var postedAt: Date
     public var postedBy: UUID
+    public var latitude: Double?
+    public var longitude: Double?
     public var image: Data
 
-    public init(posterPosition: UUID, postedAt: Date, postedBy: UUID, image: Data) {
+    public init(posterPosition: UUID, postedAt: Date, postedBy: UUID, latitude: Double? = nil, longitude: Double? = nil, image: Data) {
         self.posterPosition = posterPosition
         self.postedAt = postedAt
         self.postedBy = postedBy
+        self.longitude = longitude
+        self.latitude = latitude
         self.image = image
     }
 }
