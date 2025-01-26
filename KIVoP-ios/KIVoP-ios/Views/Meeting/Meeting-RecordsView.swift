9720831b511011d712c54229980bd87853b9da50
Adrian Paus <adrian.paus@hsrw.org>
2025-01-26 13:37:12 +0100

    Die Sprache des Protokolls wird ausgeschrieben angezeigt. Es gibt eine WatingView, falls noch kein Protokoll veröffentlicht wurde - #449 - @30038

--- KIVoP-ios/KIVoP-ios/Views/Meeting/Meeting-RecordsView.swift
+++ KIVoP-ios/KIVoP-ios/Views/Meeting/Meeting-RecordsView.swift
@@ -20,7 +20,7 @@ struct MeetingRecordsView: View {
                 } else {
                     ForEach(recordManager.records, id: \.lang) { record in
                         NavigationLink(destination: MarkdownEditorView(meetingId: record.meetingId, lang: record.lang)) {
-                            Text("Protokoll: \(record.lang)")
+                            Text("Protokoll: \(getLanguage(langCode: record.lang))")
                         }
                     }
                 }
@@ -32,5 +32,39 @@ struct MeetingRecordsView: View {
         }
     }
 
-    
+    private func getLanguage(langCode: String) -> String {
+        let languages: [(name: String, code: String)] = [
+            ("Arabisch", "ar"),
+            ("Chinesisch", "zh"),
+            ("Dänisch", "da"),
+            ("Deutsch", "de"),
+            ("Englisch", "en"),
+            ("Französisch", "fr"),
+            ("Griechisch", "el"),
+            ("Hindi", "hi"),
+            ("Italienisch", "it"),
+            ("Japanisch", "ja"),
+            ("Koreanisch", "ko"),
+            ("Niederländisch", "nl"),
+            ("Norwegisch", "no"),
+            ("Polnisch", "pl"),
+            ("Portugiesisch", "pt"),
+            ("Rumänisch", "ro"), // Hinzugefügt
+            ("Russisch", "ru"),
+            ("Schwedisch", "sv"),
+            ("Spanisch", "es"),
+            ("Thai", "th"), // Hinzugefügt
+            ("Türkisch", "tr"),
+            ("Ungarisch", "hu")
+        ]
+
+
+        // Suche nach dem Kürzel und gib den Namen zurück
+        if let language = languages.first(where: { $0.code == langCode }) {
+            return language.name
+        }
+
+        // Standardwert, falls das Kürzel nicht gefunden wird
+        return langCode
+    }
 }
