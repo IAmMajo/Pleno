229d8e1602f8adf640149127ad9e22fc7a8ddf38
30054 <henrik.peltzer@hsrw.org>
2025-01-27 12:47:35 +0100

    Adding EventRideView

--- KIVoP-ios/KIVoP-ios/Views/Ride/EventRideView/EventRideView.swift
+++ KIVoP-ios/KIVoP-ios/Views/Ride/EventRideView/EventRideView.swift
@@ -1,9 +1,143 @@
 import SwiftUI
+import CoreLocation
 
+// Das ist die Übersicht von einem Event, in der alle Fahrten zu einem Event angezeigt werden.
 struct EventRideView: View {
-    @ObservedObject var viewModel: EventRideViewModel
+    @StateObject var viewModel: EventRideViewModel
+    
+    // Vars um Standort zu kopieren
+    @State private var shareLocation = false
+    @State private var isGoogleMapsInstalled = false
+    @State private var isWazeInstalled = false
+    @State private var showMapOptions: Bool = false
+    @State private var setKoords: CLLocationCoordinate2D?
+    @State private var setAddress: String?
+    private func formattedShareText() -> String {
+       """
+       \(setAddress ?? "")
+       """
+    }
     
     var body: some View {
-        Text("Planned Ride for \(viewModel.event.name)")
+        NavigationStack{
+            ZStack {
+                // grauer Hintergrund
+                Color.gray.opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+                // Inhalt
+                VStack{
+                    Text(viewModel.formattedDate(viewModel.event.starts))
+                        .padding(5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        .padding(.vertical)
+                    
+                    List {
+                        if (viewModel.eventDetails != nil){
+                            Section(header: Text("Event Informationen")){
+                                Text(viewModel.eventDetails?.description ?? "Keine Beschreibung zu diesem Event vorhanden.")
+                            }
+                            Section(header: Text("Adresse")){
+                                VStack(alignment: .center){
+                                    Text(viewModel.address)
+                                        .foregroundColor(.blue)
+                                        .onTapGesture {
+                                            showMapOptions = true
+                                            setKoords = CLLocationCoordinate2D(
+                                                latitude: CLLocationDegrees(viewModel.eventDetails!.latitude),
+                                                longitude: CLLocationDegrees(viewModel.eventDetails!.longitude)
+                                            )
+                                            setAddress = viewModel.address
+                                        }
+                                        .confirmationDialog("Standort außerhalb der Anwendung öffnen?", isPresented: $showMapOptions) {
+                                           Button("Öffnen mit Apple Maps") {
+                                              NavigationAppHelper.shared.openInAppleMaps(
+                                                name: setAddress,
+                                                coordinate: setKoords!
+                                              )
+                                           }
+                                           if isGoogleMapsInstalled {
+                                              Button("Öffnen mit Google Maps") {
+                                                 NavigationAppHelper.shared.openInGoogleMaps(name: setAddress, coordinate: setKoords!)
+                                              }
+                                           }
+                                           if isWazeInstalled {
+                                              Button("Öffnen mit Waze") {
+                                                 NavigationAppHelper.shared.openInWaze(coordinate: setKoords!)
+                                              }
+                                           }
+                                           Button("Teilen...") {
+                                              shareLocation = true
+                                           }
+                                           Button("Abbrechen", role: .cancel) {}
+                                        }
+                                    Divider()
+                                        .background(Color.gray)
+                                        .padding(.bottom, 10)
+                                    Text("\(viewModel.eventDetails!.latitude), \(viewModel.eventDetails!.longitude)")
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                            .onAppear(){
+                                viewModel.getAddressFromCoordinates(latitude: viewModel.eventDetails!.latitude, longitude: viewModel.eventDetails!.longitude) { address in
+                                    if let address = address {
+                                        viewModel.address = address
+                                    }
+                                }
+                            }
+                        }
+                        if (viewModel.event.myState != .present){
+                            Text("Du hast dem ausgewählten Event nicht zugesagt, und kannst daher keiner Fahrgemeinschaft beitreten.")
+                                .padding(.horizontal)
+                            Button(action: {
+                                viewModel.participateEvent()
+                            }){
+                                Text("Jetzt teilnehmen")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.horizontal)
+                            .buttonStyle(PlainButtonStyle())
+                        } else {
+                            Section(header: Text("Fahrer")){
+                                ForEach( viewModel.eventRides, id: \.id ) { ride in
+                                    Text("Ride: \(ride.driverName)")
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(.insetGrouped)
+                }
+                .overlay {
+                    if viewModel.isLoading {
+                        ProgressView("Lädt...")
+                    }
+                }
+                .onAppear {
+                    Task {
+                        viewModel.fetchEventDetails()
+                        viewModel.fetchEventRides()
+                    }
+                }
+                .refreshable {
+                    Task {
+                        viewModel.fetchEventDetails()
+                        viewModel.fetchEventRides()
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $shareLocation) {
+            ShareSheet(activityItems: [formattedShareText()])
+               .presentationDetents([.medium, .large])
+               .presentationDragIndicator(.hidden)
+        }
+        .navigationTitle(viewModel.event.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
--- KIVoP-ios/KIVoP-ios/Views/Ride/EventRideView/EventRideViewModel.swift
+++ KIVoP-ios/KIVoP-ios/Views/Ride/EventRideView/EventRideViewModel.swift
@@ -1,10 +1,252 @@
 import Foundation
+import CoreLocation
 import RideServiceDTOs
 
 class EventRideViewModel: ObservableObject {
-    var event: GetEventDTO
+    @Published var event: GetEventDTO
+    @Published var eventDetails: GetEventDetailDTO?
+    @Published var eventRides: [GetEventRideDTO] = []
+    @Published var isLoading: Bool = false // Standardmäßig nicht ladend
     
-    init(event: GetEventDTO){
+    @Published var address: String = ""
+    
+    private let baseURL = "https://kivop.ipv64.net"
+    
+    init(event: GetEventDTO) {
         self.event = event
+    }
+    
+    // Weitere Details zum Event bekommen
+    func fetchEventDetails() {
+        guard let url = URL(string: "\(baseURL)/events/\(event.id)") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Füge JWT Token zu den Headern hinzu
+        if let token = UserDefaults.standard.string(forKey: "jwtToken") {
+            request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        } else {
+            print("Unauthorized: No token found")
+            return
+        }
+        
+        // Setze den Ladevorgang auf true
+        DispatchQueue.main.async {
+            self.isLoading = true
+        }
+        
+        // Führe den Netzwerkaufruf aus
+        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            // Behandle Fehler
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, auch bei Fehlern
+                }
+                print("Network error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, keine Daten
+                }
+                print("No data received from server")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                // Decodieren der Antwort
+                let decodedEventDetails = try decoder.decode(GetEventDetailDTO.self, from: data)
+                DispatchQueue.main.async {
+                    self.eventDetails = decodedEventDetails
+                    self.isLoading = false // Ladevorgang erfolgreich beendet
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, aber mit JSON-Fehler
+                }
+                print("JSON Decode Error: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    // Alle Event Fahrten bekommen
+    func fetchEventRides() {
+        guard let url = URL(string: "\(baseURL)/eventrides?byEventID=\(event.id)") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Füge JWT Token zu den Headern hinzu
+        if let token = UserDefaults.standard.string(forKey: "jwtToken") {
+            request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        } else {
+            print("Unauthorized: No token found")
+            return
+        }
+        
+        // Setze den Ladevorgang auf true
+        DispatchQueue.main.async {
+            self.isLoading = true
+        }
+        
+        // Führe den Netzwerkaufruf aus
+        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            // Behandle Fehler
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, auch bei Fehlern
+                }
+                print("Network error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, keine Daten
+                }
+                print("No data received from server")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                // Decodieren der Antwort
+                let decodedEventRides = try decoder.decode([GetEventRideDTO].self, from: data)
+                DispatchQueue.main.async {
+                    self.eventRides = decodedEventRides
+                    self.isLoading = false // Ladevorgang erfolgreich beendet
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.isLoading = false // Ladevorgang beendet, aber mit JSON-Fehler
+                }
+                print("JSON Decode Error: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    func participateEvent() {
+        self.isLoading = true
+        
+        guard let url = URL(string: "\(baseURL)/events/\(event.id)/participations") else {
+            print("Invalid URL")
+            self.isLoading = false
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Füge JWT Token hinzu oder beende bei Fehler
+        guard let token = UserDefaults.standard.string(forKey: "jwtToken") else {
+            print("Unauthorized: No token found")
+            self.isLoading = false
+            return
+        }
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let requestBody: [String: Any] = ["participates": true]
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+            request.httpBody = jsonData
+        } catch {
+            print("Fehler beim Serialisieren des JSON: \(error.localizedDescription)")
+            self.isLoading = false
+            return
+        }
+        
+        // Führe den Netzwerkaufruf aus
+        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+            }
+            
+            if let error = error {
+                print("Network error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received from server")
+                return
+            }
+            
+            // Debugging: Zeige die Antwort als String
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("Server Response: \(jsonString)")
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.event.myState = .present
+                    self.isLoading = false
+                }
+            }
+        }.resume()
+    }
+    
+    func getAddressFromCoordinates(latitude: Float, longitude: Float, completion: @escaping (String?) -> Void) {
+        let clLocation = CLLocation(latitude: Double(latitude), longitude: Double(longitude))
+        
+        CLGeocoder().reverseGeocodeLocation(clLocation) { placemarks, error in
+            if let error = error {
+                print("Geocoding error: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                completion(nil)
+                return
+            }
+            
+            var addressString = ""
+            if let name = placemark.name {
+                addressString += name
+            }
+            if let postalCode = placemark.postalCode {
+                addressString += ", \(postalCode)"
+            }
+            if let city = placemark.locality {
+                addressString += " \(city)"
+            }
+//            if let country = placemark.country {
+//                addressString += ", \(country)"
+//            }
+            
+            completion(addressString)
+        }
+    }
+    
+    func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy - HH:mm 'Uhr'"
+        return formatter.string(from: date)
     }
 }
