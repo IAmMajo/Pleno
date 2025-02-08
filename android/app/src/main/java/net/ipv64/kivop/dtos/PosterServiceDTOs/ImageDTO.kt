3c33934f9096e9cc8c4642d8952fa6b887b68798
KIVoP Debian Gitlab Runner <lennart-luis.guggenberger@hsrw.org>
2025-02-08 16:20:23 +0100

    Kotlin-DTOs automatisch generiert

--- android/app/src/main/java/net/ipv64/kivop/dtos/PosterServiceDTOs/ImageDTO.kt
+++ android/app/src/main/java/net/ipv64/kivop/dtos/PosterServiceDTOs/ImageDTO.kt
@@ -3,8 +3,6 @@ package net.ipv64.kivop.dtos.PosterServiceDTOs
 import java.util.UUID
 import java.time.LocalDateTime
 
-data class PosterResponseDTO (
-    var id : UUID,
-    var name : String,
-    var description : String?,
+data class ImageDTO (
+    var image : String,
 )
